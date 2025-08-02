5b2055301ffdce328d27b9bbdde3c02bb6f815b9
jimmyc29 <jcocb4@miumg.edu.gt>
2025-08-02 15:45:06 -0600

    :rocket: Commit base del proyecto de calculadora

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenido a la Calculadora");
+        Console.WriteLine("1. Suma");
+        Console.WriteLine("2. Resta");
+        Console.WriteLine("3. Multiplicación");
+        Console.WriteLine("4. División");
+        Console.Write("Seleccione una opción: ");
+
+        int opcion = int.Parse(Console.ReadLine());
+        // TODO: Implementar la lógica de menú
+    }
+
+    // TODO: Implementar funciones de suma, resta, multiplicación, división
+}
