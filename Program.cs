c5f65df284ca87db02a6b558733e7c146cdc72cb
Herberth Alvarez <garciaguerber@gmail.com>
2025-08-02 19:45:51 -0600

    agrege la opcion de suma y la llame desde la funcion

--- Program.cs
+++ Program.cs
@@ -13,7 +13,27 @@ class Program
 
         int opcion = int.Parse(Console.ReadLine());
         // TODO: Implementar la lógica de menú
+       switch (opcion){
+        case 1: 
+        Console.WriteLine("seleccionastes Opcion Sumar");
+        Console.WriteLine("ingrese el primer numero");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("ingrese el segundo numero");
+        int num2= Convert.ToInt32(Console.ReadLine());
+
+        int resultado = Suma (num1 + num2);
+        Console.WriteLine("resultado: " + resultado);
+        break;
+
+     default:
+        Console.WriteLine("Opción no implementada aún.");
+        break;
+       }
     }
 
     // TODO: Implementar funciones de suma, resta, multiplicación, división
+    static int Suma(int a, int b){
+         return a+b;
+    }
 }
